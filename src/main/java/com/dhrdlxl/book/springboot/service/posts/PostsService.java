2385ffbf98c932a7af646e1735beea53faaecd02
SeungHyunYoo <dhrdlxl@naver.com>
2022-10-20 22:42:40 +0900

    게시글 등록 API 생성

---
+++ src/main/java/com/dhrdlxl/book/springboot/service/posts/PostsService.java
@@ -0,0 +1,18 @@
+package com.dhrdlxl.book.springboot.service.posts;
+
+import com.dhrdlxl.book.springboot.domain.posts.PostsRepository;
+import com.dhrdlxl.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto postsSaveRequestDto) {
+        return postsRepository.save(postsSaveRequestDto.toEntity()).getId();
+    }
+}
